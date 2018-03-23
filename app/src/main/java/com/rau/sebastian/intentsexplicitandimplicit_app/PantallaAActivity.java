d09960a09ee3acef683386bf8f251c0cf5e3be11
Sebastian <sebastian.rau@tecsup.edu.pe>
2018-03-23 06:06:49 -0500

    Initial commit

---
+++ app/src/main/java/com/rau/sebastian/intentsexplicitandimplicit_app/PantallaAActivity.java
@@ -0,0 +1,13 @@
+package com.rau.sebastian.intentsexplicitandimplicit_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_a);
+    }
+}
